f4d458c2304d3426b5aea2d9f7a4a93660a0b42c
willsharpless <williamsharpless.2@gmail.com>
2025-06-23 21:39:24 +0000

    RR anim works, but traj snap shot fig is hard...

--- render/render_hopper_RR.py
+++ render/render_hopper_RR.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import re
+import json
 import trimesh
 from copy import deepcopy 
 import numpy as np
@@ -17,145 +18,14 @@ from rraa_rl.EFPPO.src.env.env_list import get_env
 import imageio
 
 from brax.io import html
-from brax.v1.io import html as html1
+# from brax.v1.io import html as html1
 
 def load_traj(file_path):
     with jnp.load(file_path, allow_pickle=False) as traj_data:
         traj_batch = {key: traj_data[key] for key in traj_data.files}
     return traj_batch
 
-# def _draw_f16_scene(position, euler_angles, setting_type, mesh, scale, trail=None, title=None, title2='Body', set_follow_cam=True):
-#     fig = plt.figure(dpi=200)
-#     gs = gridspec.GridSpec(1, 3)
-#     ax = fig.add_subplot(gs[0, :2], projection='3d')
-
-#     # Main scene
-#     draw_targets(ax, setting_type=setting_type)
-#     plot_mesh(ax, mesh, pos=position, euler_angles=euler_angles, scale=scale, set_follow_cam=set_follow_cam)
-
-#     # Add trajectory trail (if given)
-#     if trail is not None and len(trail) > 1:
-#         trail = np.array(trail)
-#         ax.plot3D(trail[:, 0], trail[:, 1], trail[:, 2], linestyle='--', color='black', linewidth=0.5, alpha=0.7)
-
-#     # CONFIGURE PLOT
-#     if setting_type == 'RR':
-#         ax.set_xlim(0, 1600)
-#         ax.set_ylim(-400, 400)
-#         ax.set_zlim(100, 1000)
-#         ax.set_xticks([0, 200, 400, 600, 800, 1000, 1200, 1400, 1600])
-#         ax.set_xticklabels(['0', '', '', '', '', '', '', '', '1600'])
-#         ax.set_yticks([-400, -200, 0, 200, 400])
-#         ax.set_yticklabels(['-400', '', '0', '', '400'])
-#         ax.set_zticks([100, 300, 500, 700, 900, 1100])
-#         ax.set_zticklabels(['100', '', '', '', '', '1100'])
-#     elif setting_type == 'RAA':
-#         ax.set_xlim(0, 2100)
-#         ax.set_ylim(-550, 550)
-#         ax.set_zlim(-50, 1000)
-#         ax.set_xticks([0, 400, 800, 1200, 1600, 2000])
-#         ax.set_xticklabels(['0', '', '', '', '', '2000'])
-#         ax.set_yticks([-500, -250, 0, 250, 500])
-#         ax.set_yticklabels(['-500', '', '0', '', '500'])
-#         ax.set_zticks([0, 200, 400, 600, 800, 1100])
-#         ax.set_zticklabels(['0', '', '', '', '', '1100'])
-
-#     ax.set_xlabel("Position North")
-#     ax.set_ylabel("Position East")
-#     ax.set_zlabel("Altitude")
-#     ax.zaxis.set_label_coords(-0.5, 0.)
-#     ax.set_aspect('equal')
-
-#     bg_color = "#e6ecf2"
-#     for axis in [ax.xaxis, ax.yaxis, ax.zaxis]:
-#         axis.set_pane_color(plt.matplotlib.colors.to_rgba(bg_color))
-#         axis._axinfo["grid"]['color'] = (1, 1, 1, 1)
-
-#     # Close-up view
-#     ax2 = fig.add_subplot(gs[0, 2], projection='3d')
-#     plot_mesh(ax2, mesh, pos=position, euler_angles=euler_angles, scale=scale, alpha=1.0, set_follow_cam=set_follow_cam)
-#     draw_targets(ax2, setting_type=setting_type, alpha=0.2, draw_obstacles=False)
-#     ax2.set_xticklabels([])
-#     ax2.set_yticklabels([])
-#     ax2.set_zticklabels([])
-#     span = 30 * 2
-#     ax2.set_xlim(position[0] - span/2, position[0] + span/2)
-#     ax2.set_ylim(position[1] - span/2, position[1] + span/2)
-#     ax2.set_zlim(position[2] - span/2, position[2] + span/2)
-
-#     for axis in [ax2.xaxis, ax2.yaxis, ax2.zaxis]:
-#         axis.set_pane_color(plt.matplotlib.colors.to_rgba(bg_color))
-#         axis._axinfo["grid"]['color'] = (1, 1, 1, 1)
-
-#     ax2.set_title(title2)
-#     ax2.set_aspect('equal')
-
-#     # FINAL TOUCHES
-#     plt.subplots_adjust(left=0.01, right=0.99, top=0.99, bottom=0.01)
-#     plt.subplots_adjust(wspace=0.5)
-#     plt.suptitle(title, fontsize=20, y=0.95)
-
-#     return fig
-
-# def render_f16_trajectory_gif(position_traj, angles_traj, setting_type, mesh, scale, output_gif_path, temp_dir="frames", dpi=200, config=None):
-#     os.makedirs(temp_dir, exist_ok=True)
-#     frame_paths = []
-#     print(f"Rendering F16 trajectory GIF to {output_gif_path}...")
-
-#     if config is not None and config['ALG'] == 'DOHJPPO':
-#         title = r'$\mathtt{F16}$ — RR — $\mathbf{DO\text{-}HJ\text{-}PPO}$'
-#     elif config is not None and 'CPPO' in config['ALG']:
-#         title = r'$\mathtt{F16}$ — RR — $\mathbf{C\text{-}PPO}$'
-#     elif config is not None and 'DSTL' in config['ALG']:
-#         title = r'$\mathtt{F16}$ — RR — $\mathbf{D\text{-}STL}$'
-
-#     for t in range(len(position_traj)):
-#         print(f"Rendering frame {t+1}/{len(position_traj)}")
-#         trail = position_traj[:t+1]  # slice up to current time
-#         fig = _draw_f16_scene(position_traj[t], angles_traj[t], setting_type, mesh, scale, trail=trail, title=title)
-#         frame_path = os.path.join(temp_dir, f"frame_{t:04d}.png")
-#         fig.savefig(frame_path, dpi=dpi)
-#         plt.close(fig)
-#         frame_paths.append(frame_path)
-
-#     # Create GIF
-#     images = [imageio.v3.imread(fp) for fp in frame_paths]
-#     imageio.mimsave(output_gif_path, images, duration=0.05)
-
-#     # Optional cleanup
-#     for fp in frame_paths:
-#         os.remove(fp)
-#     os.rmdir(temp_dir)
-
-# def render_f16_trajectory_png(position_traj, angles_traj, setting_type, mesh, scale, output_png_path, dpi=200, config=None):
-
-#     if config is not None and config['ALG'] == 'DOHJPPO':
-#         title = r'$\mathtt{F16}$ — RR — $\mathbf{DO\text{-}HJ\text{-}PPO}$'
-#     elif config is not None and 'CPPO' in config['ALG']:
-#         title = r'$\mathtt{F16}$ — RR — $\mathbf{C\text{-}PPO}$'
-#     elif config is not None and 'DSTL' in config['ALG']:
-#         title = r'$\mathtt{F16}$ — RR — $\mathbf{D\text{-}STL}$'
-
-#     # Draw final scene with trail
-#     t = len(position_traj) - 1
-#     trail = position_traj[:t+1]  # slice up to current time
-#     fig = _draw_f16_scene(position_traj[t], angles_traj[t], setting_type, mesh, scale, 
-#                           trail=trail, title=title, title2='Final Body', set_follow_cam=False)
-    
-#     # Draw intermediate scenes on fig
-#     ax = fig.axes[0]
-#     snapshot_sample_indices = [0, 25, 50, 100, 150]
-#     # snapshot_sample_indices = [0, 25, 50, 100]
-#     for t in snapshot_sample_indices:
-#         plot_mesh(ax, mesh, pos=position_traj[t], euler_angles=angles_traj[t], scale=scale, set_follow_cam=False)
-        
-#     fig.savefig(output_png_path, dpi=dpi)
-#     plt.close(fig)
-
-def inject_custom_script(html_str):
-    pattern = r'<script type="module">.*?</script>'
-
-    custom_script = """
+HTML_ANIMATION_SCRIPT = """
     <script type="module">
     import * as THREE from 'three';
     import { Viewer } from 'viewer';
@@ -309,6 +179,9 @@ def inject_custom_script(html_str):
     </script>
 """.strip()
 
+def inject_custom_script(html_str, custom_script):
+    pattern = r'<script type="module">.*?</script>'
+
     modified_html = re.sub(pattern, custom_script, html_str, flags=re.DOTALL)
     return modified_html
 
@@ -320,12 +193,20 @@ if __name__ == "__main__":
     config = vars(get_args(sys.argv[1:]))
     config["EXP_NAME"]="HopperReachReach"
     config["PROBLEM_TYPE"]="RR"
-    config["ALG"]="DOHJPPO"
+    config["ALG"]="DSTL" # DOHJPPO, CPPOvI, DSTL
     fig_file_name = f"render/gifs/hopper_RR_trajectory_render_{config['ALG']}_{sample_index}_view0_{np.random.randint(100000):2d}" #FIXME randint for touch bug
     traj_batch = load_traj(f"model/eval_all_figs/Hopper_RR_061925/traj_sample/traj_{config['ALG']}.npz")
 
     envs = get_env(config)
     env = envs[0]
+
+    ## Compute important points
+
+    reached_1 = traj_batch['reach1'][:, sample_index] < 0.
+    reached_2 = traj_batch['reach2'][:, sample_index] < 0.
+    first_reached_1 = np.where(reached_1)[0][0] if np.any(reached_1) else traj_batch['obs'].shape[0] - 1
+    first_reached_2 = np.where(reached_2)[0][0] if np.any(reached_2) else traj_batch['obs'].shape[0] - 1
+    first_reached_both = np.max([first_reached_1, first_reached_2])
 
     ## Render single step
 
@@ -334,58 +215,170 @@ if __name__ == "__main__":
     # test_obs[1] = test_obs[1] - 1.25
     # qpos = test_obs[:6]
     # qvel = test_obs[6:12]
-    # pipeline_state = env._env._env.env.env.pipeline_init(qpos, qvel)
+    # init_pipeline_state = env._env._env.env.env.pipeline_init(qpos, qvel)
     
     # html_str = html.render(
     #     sys=env._env._env.env.env.sys,
-    #     states=[pipeline_state],
+    #     states=[init_pipeline_state],
     #     height=720,  # optional
     # )
 
     ## Render trajectory snapshot
 
-    renderer = html1.Renderer(env._env._env.env.env.sys)
+    # renderer = html1.Renderer(env._env._env.env.env.sys)
 
-    reached_both = traj_batch['has_reached_1'][:, sample_index] * traj_batch['has_reached_2'][:, sample_index]
-    first_reached_both = np.where(reached_both)[0] if np.any(reached_both) else traj_batch['obs'].shape[0] - 1
-    interval = first_reached_both // 5 if first_reached_both > 5 else 1
+    num_bodies_approx = 5
+    interval = first_reached_both // num_bodies_approx if first_reached_both > num_bodies_approx else 1
 
-    default_color = env.sys.link_color
-    default_r, default_g, default_b = default_color[0][0], default_color[0][1], default_color[0][2]
-
+    # default_color = env.sys.link_color
+    # default_r, default_g, default_b = default_color[0][0], default_color[0][1], default_color[0][2]
+    
+    # qpos_list, qvel_list = [], []
+    pipeline_states = []
+    steps_saved = []
+    reached_1_list, reached_2_list, alpha_list = [], [], []
     for step_i in range(traj_batch['obs'].shape[0]):
         
-        at_interval = step_i % interval == 0 or step_i == first_reached_both
-        if not at_interval or traj_batch['has_reached_1'][step_i, sample_index] or traj_batch['has_reached_2'][step_i, sample_index]:
+        at_interval = step_i % interval == 0
+        if not (at_interval or step_i in [first_reached_1, first_reached_2, first_reached_both]) and step_i != 1:
             continue
+        if step_i == 0: # bad rendering
+            continue
+        steps_saved.append(step_i)
 
         test_obs = traj_batch['obs'][step_i, sample_index, :]
         test_obs[1] = test_obs[1] - 1.25
         qpos = test_obs[:6]
         qvel = test_obs[6:12]
+
         pipeline_state = env._env._env.env.env.pipeline_init(qpos, qvel)
-        # pipeline_states.append(pipeline_state)
+        pipeline_states.append(pipeline_state)
         
-        alpha_range = 0.2  # range for alpha values
+        ## alpha and color
+        alpha_range = 0.8  # range for alpha values
         # alpha_i = 1.0 - alpha_range * (step_i / first_reached_both) # decreasing
         alpha_i = (1 - alpha_range) + alpha_range * (step_i / first_reached_both) # increasing
+        alpha_list.append(alpha_i)
 
-        color = (default_r, default_g, default_b, alpha_i)  # blue with decreasing alpha
-        if traj_batch['has_reached_1'][step_i, sample_index]: 
-            color = (0.0, 1.0, 0.0, 1.0)  # green with full alpha
-        elif traj_batch['has_reached_2'][step_i, sample_index]:
-            color = (0.0, 0.0, 1.0, 1.0)  # red with full alpha
+        # color = (default_r, default_g, default_b, alpha_i)  # blue with decreasing alpha
+        if step_i == first_reached_1: 
+            # color = (0.0, 1.0, 0.0, 1.0)  # green with full alpha
+            reached_1_list.append(1)
+        else:
+            reached_1_list.append(0)
+          
+        if step_i == first_reached_2:
+            reached_2_list.append(1)
+            # color = (0.0, 0.0, 1.0, 1.0)  # red with full alpha
+        else:
+            reached_2_list.append(0)
 
-        renderer.add(pipeline_state, color=color)
-
-        if traj_batch['has_reached_1'][step_i, sample_index] and traj_batch['has_reached_2'][step_i, sample_index]:
+        # renderer.add(pipeline_state, color=color)
+        if step_i == first_reached_both:
             break
 
-    html_str = html1.render(renderer.scene)
-    html_str = inject_custom_script(html_str)
+    os.makedirs(f"render/hopper_{config['PROBLEM_TYPE']}_snapshots_{config['ALG']}_seed{sample_index}", exist_ok=True)
+    for i, state_i in enumerate(pipeline_states):
+        html_str = html.render(
+            sys=env._env._env.env.env.sys,
+            states=[state_i],
+            height=720,  # optional
+        )
+        html_str = inject_custom_script(html_str, HTML_ANIMATION_SCRIPT)
+        with open(f"render/hopper_{config['PROBLEM_TYPE']}_snapshots_{config['ALG']}_seed{sample_index}/snap_{i}.html", "w") as f:
+            f.write(html_str)
 
-    with open("render/hopper_render_test.html", "w") as f:
-        f.write(html_str)
+    # html_str = html.render(
+    #     sys=env._env._env.env.env.sys,
+    #     states=[init_pipeline_state],
+    #     height=720,
+    # )
+    # Serialize trajectory as JSON (for injection into HTML)
+    
+    # traj_json = json.dumps([
+    #     {'qpos': qpos, 'qvel': qvel}
+    #     for qpos, qvel in zip(qpos_list, qvel_list)
+    # ])
+    
+    # qpos_json = json.dumps([q.tolist() for q in qpos_list])
+    # qvel_json = json.dumps([v.tolist() for v in qvel_list])
+
+    # poses_json = json.dumps([state.x.pos.tolist() for state in pipeline_states])
+    # rotes_json = json.dumps([state.x.rot.tolist() for state in pipeline_states])
+
+    # reached_json = json.dumps([
+    #     {'reached1': reached1, 'reached2': reached2}
+    #     for reached1, reached2 in zip(reached_1_list, reached_2_list)
+    # ])
+
+    # alpha_json = json.dumps([
+    #     {'alpha': alpha}
+    #     for alpha in alpha_list
+    # ])
+
+    # html_str = html1.render(renderer.scene)
+
+    ## Inject trajectory data into script
+
+    # Inject custom <script type="module"> block
+    # html_snapshot_script = f"""
+    # <script type="module">
+    #   import * as THREE from 'three';
+    #   import {{ Viewer }} from 'viewer';
+
+    #   const domElement = document.getElementById("brax-viewer");
+    #   const viewer = new Viewer(domElement, system);
+
+    #   const poses = {poses_json};
+    #   const rotes = {rotes_json};
+    #   const reachs = {reached_json};
+    #   const alphas = {alpha_json};
+
+    #   const interval = setInterval(() => {{
+    #     if (!viewer.scene || !viewer.renderer || !viewer.camera || !viewer.controls) return;
+
+    #     // Step 1: Locate original body parts by filtering visible mesh objects
+    #     const originalBodyMeshes = viewer.scene.children.filter(obj =>
+    #       obj.type === 'Mesh' && obj.name !== 'ground' && obj.visible
+    #     );
+    #     console.log('originalBodyMeshes', originalBodyMeshes)
+
+    #     // Step 2: Create transparent clones for each timestep
+    #     for (let t = 0; t < poses.length; t++) {{
+    #       const pose_t = poses[t];
+    #       const rot_t = rotes[t];
+
+    #       for (let j = 0; j < pose_t.length; j++) {{
+    #         if (!originalBodyMeshes[j]) continue;
+
+    #         const clone = originalBodyMeshes[j].clone();
+    #         clone.position.fromArray(pose_t[j]);
+    #         clone.quaternion.fromArray(rot_t[j]);
+
+    #         // Set transparent material
+    #         clone.material = clone.material.clone();
+    #         clone.material.transparent = true;
+    #         clone.material.opacity = alphas[t];  // adjust for fading effect
+
+    #         // Optional: prevent z-fighting / sorting issues
+    #         clone.renderOrder = t;
+
+    #         viewer.scene.add(clone);
+    #       }}
+    #     }}
+
+    #     clearInterval(interval);
+    #   }}, 100);
+    # </script>
+    # """
+
+    # Inject script into the HTML before closing </body>
+    # html_with_traj = html_str.replace("</body>", custom_script + "\n</body>")
+
+    # html_str = inject_custom_script(html_str, html_snapshot_script)
+
+    # with open("render/hopper_snapshot_test.html", "w") as f:
+    #     f.write(html_str)
 
     if draw_gif:
         
@@ -399,7 +392,7 @@ if __name__ == "__main__":
             pipeline_state = env._env._env.env.env.pipeline_init(qpos, qvel)
             pipeline_states.append(pipeline_state)
 
-            if traj_batch['has_reached_1'][step_i, sample_index] and traj_batch['has_reached_2'][step_i, sample_index]:
+            if step_i == first_reached_both:
                 break
 
         html_str = html.render(
@@ -407,6 +400,6 @@ if __name__ == "__main__":
             states=pipeline_states,
             height=720,  # optional
         )
-        html_str = inject_custom_script(html_str)
+        html_str = inject_custom_script(html_str, HTML_ANIMATION_SCRIPT)
         with open("render/hopper_render_anim_RR.html", "w") as f:
             f.write(html_str)
