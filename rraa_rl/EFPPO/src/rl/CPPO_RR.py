7467c42e74b307f04de4d1b759f08e767e8a8613
willsharpless <williamsharpless.2@gmail.com>
2025-07-24 20:56:39 +0000

    CPPO RR wandb fix

--- rraa_rl/EFPPO/src/rl/CPPO_RR.py
+++ rraa_rl/EFPPO/src/rl/CPPO_RR.py
@@ -219,18 +219,18 @@ def train(env, env_params, config, rng):
                     "Reach 1 Success %": reach_1_perc,
                     "Reach 2 Success %": reach_2_perc,
                     "Reach-Reach Success %": reach_perc,
-                   "lambda": jnp.mean(loss_info['lambda'])})
+                   "lambda": jnp.mean(loss_info['lambda'])}, step=timestep)
         
         if "Hopper" in config["EXP_NAME"] or "HalfCheetah" in config["EXP_NAME"]:
                 wandb.log({
-                    'trajectory_sample':wandb.Image(fig),
-                })
+                    'trajectory_sample':wandb.Image(fig)
+                }, step=timestep)
         
         # Save video of trajectory 
         video_freq = 5 #25 
         save_video = True 
         if timestep % video_freq == 0 or timestep == total_timesteps - 1: 
-            video_frames = plot_video_contour_RRAA((info, None, None), timestep + 1, config, save_video=save_video, log_wandb=config["USE_WANDB"])
+            video_frames = plot_video_contour_RRAA((info, None, None), timestep, config, save_video=save_video, log_wandb=config["USE_WANDB"])
 
         ####### RRAA Change ######
         print("Iteration {}: not reach 1 {} not reach 2 {} not reach both {} reward {} cost {}".format(timestep, cnt1, cnt2, cnt3, -jnp.mean(reward), jnp.mean(cost)))
