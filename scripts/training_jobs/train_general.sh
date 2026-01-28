f55391c3bdb2c185c0bad9f3b3b2603c6dafff22
willsharpless <williamsharpless.2@gmail.com>
2026-01-28 05:33:32 -0500

    fck sorry os (fxd train_general)

--- scripts/training_jobs/train_general.sh
+++ scripts/training_jobs/train_general.sh
@@ -4,12 +4,12 @@ alg=${1:-vd}  # Use first argument, default to 'vd'
 GPU_ID=${2:-0}
 env_name=${3:-delivery}
 
-screen -dmS gpu1_job bash -c "
+screen -dmS gpu1_job bash -c '
 export CUDA_VISIBLE_DEVICES=${GPU_ID}
 
 python scripts/train.py '"${alg}"' --env_name '"${env_name}"' --seed 0 --name '"${env_name}"'_'"${alg}"'_seed0
 python scripts/train.py '"${alg}"' --env_name '"${env_name}"' --seed 1 --name '"${env_name}"'_'"${alg}"'_seed1
 python scripts/train.py '"${alg}"' --env_name '"${env_name}"' --seed 2 --name '"${env_name}"'_'"${alg}"'_seed2
-"
+'
 
 echo "jobs have begun on GPU ${GPU_ID}"
